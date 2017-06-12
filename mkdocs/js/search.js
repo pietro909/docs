e7c99b295912d14533b89165203baf9fff0b5354
sahil <sahilverma1@gmail.com>
2017-06-12 15:24:13 -0800

    Deployed d9296b9 with MkDocs version: 0.16.3

--- mkdocs/js/search.js
+++ mkdocs/js/search.js
@@ -6,11 +6,8 @@ require([
 ], function (Mustache, lunr, results_template, data) {
    "use strict";
 
-   console.log('here')
-
     function getSearchTerm()
     {
-        console.log('getSearchTerm')
         var sPageURL = window.location.search.substring(1);
         var sURLVariables = sPageURL.split('&');
         for (var i = 0; i < sURLVariables.length; i++)
@@ -40,8 +37,8 @@ require([
     }
 
     var search = function(){
+
         var query = document.getElementById('mkdocs-search-query').value;
-        console.log(query)
         var search_results = document.getElementById("mkdocs-search-results");
         while (search_results.firstChild) {
             search_results.removeChild(search_results.firstChild);
@@ -52,7 +49,6 @@ require([
         }
 
         var results = index.search(query);
-        console.log(results)
 
         if (results.length > 0){
             for (var i=0; i < results.length; i++){
